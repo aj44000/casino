ede44cca9bb778989f6d832c9f164375b139c6b0
aj <aj.chludzinski@gmail.com>
2024-02-23 21:33:18 -0500

    Basic setup done

---
+++ blackjack.py
@@ -0,0 +1,16 @@
+cards = 0
+
+
+def dealer():
+    if cards <= 16:
+        # TODO Make so dealer pulls a card when their hand is less than or equal to sixteen
+        pass
+
+
+def player():
+    pass
+
+
+def test():
+    deck = ['A', 'K', 'Q', 'J', 10, 9, 8, 7, 6, 5, 4, 3, 2, 1] * 4
+    print("The deck has 4 decks of cards")
